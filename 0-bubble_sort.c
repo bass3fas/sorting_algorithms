bd2aa9a3656b755e86671900b4bff31f94f3fc2f
Bassant Aboelhassan <yaralikuslar64@gmail.com>
2023-11-17 03:07:46 -0800

    bubble sorting functioning

---
+++ 0-bubble_sort.c
@@ -0,0 +1,38 @@
+#include "sort.h"
+/**
+ * swap - changing the vlaues of 2 elements
+ * @a: first to swap
+ * @b: second to swap
+ */
+void swap(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+/**
+ * bubble_sort - sorting using bubble algorithm
+ * @array: array to be sorted
+ * @size: size of array
+ */
+void bubble_sort(int *array, size_t size)
+{
+	int swaps;
+	size_t i;
+
+	while (1)
+	{
+		swaps = 0;
+		for (i = 0; i < size - 1; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				swap(&array[i], &array[i + 1]);
+				swaps += 1;
+				print_array(array, size);
+			}
+		}
+		if (swaps == 0)
+			break;
+	}
+}
