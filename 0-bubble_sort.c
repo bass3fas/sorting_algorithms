990f673eedc1af56d7bce2cb9b0bb8571981449e
Mohanned-sameh <mohanned2678@gmail.com>
2023-11-17 19:23:07 +0200

    reverting changes

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,40 +1,32 @@
 #include "sort.h"
 /**
- * swap - changing the vlaues of 2 elements
- * @a: first to swap
- * @b: second to swap
- */
-void swap(int *a, int *b)
-{
-	int temp = *a;
-	*a = *b;
-	*b = temp;
-}
-/**
  * bubble_sort - sorting using bubble algorithm
  * @array: array to be sorted
  * @size: size of array
  */
 void bubble_sort(int *array, size_t size)
 {
-	int swaps;
-	size_t i;
+	size_t i, j;
+	int temp;
+	bool swapped;
 
 	if (array == NULL || size < 2)
 		return;
-	while (1)
+	for (i = 0; i < size - 1; i++)
 	{
-		swaps = 0;
-		for (i = 0; i < size - 1; i++)
+		swapped = false;
+		for (j = 0; j < size - i - 1; j++)
 		{
-			if (array[i] > array[i + 1])
+			if (array[j] > array[j + 1])
 			{
-				swap(&array[i], &array[i + 1]);
-				swaps += 1;
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
 				print_array(array, size);
 			}
+			swapped = true;
 		}
-		if (swaps == 0)
+		if (swapped == false)
 			break;
 	}
 }
