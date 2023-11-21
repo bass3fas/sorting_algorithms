f6564750dd9a2a772f99c346452be650533a633f
Mohanned-sameh <mohanned2678@gmail.com>
2023-11-21 03:18:24 +0200

    fixed output

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -7,34 +7,36 @@
  */
 void counting_sort(int *array, size_t size)
 {
-	int max, *count, *output;
+	int max = array[0];
+	int *count = NULL;
+	int *output = NULL;
 	size_t i, n, l;
 	int j, k;
 
 	if (size <= 1)
 		return;
-
-	max = array[0];
 	for (l = 1; l < size; ++l)
 	{
 		if (array[l] > max)
 			max = array[l];
 	}
 
-	count = calloc(max + 1, sizeof(int));
+	count = malloc((max + 1) * sizeof(int));
 	if (count == NULL)
 		return;
+	for (i = 0; i <= (size_t)max; ++i)
+		count[i] = 0;
 	for (i = 0; i < size; ++i)
 		++count[array[i]];
-	print_array(count, max + 1);
-	for (j = 1; j <= max; ++j)
-		count[j] += count[j - 1];
 	output = malloc(size * sizeof(int));
 	if (output == NULL)
 	{
 		free(count);
 		return;
 	}
+	for (j = 1; j <= max; ++j)
+		count[j] += count[j - 1];
+	print_array(count, max + 1);
 	for (k = size - 1; k >= 0; --k)
 	{
 		output[count[array[k]] - 1] = array[k];
